61f32f27327fafce4cecc3cb81a87eaf4eb7fe9e
Thijsvdzn <78796431+Thijsvdzn@users.noreply.github.com>
2022-06-16 12:42:59 +0200

    Smoother Cloud animations

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 document.getElementById('button').addEventListener('click', function () {
-    document.querySelector('#topLeftClouds').classList.add('leftMove');
-    document.querySelector('#bottomLeftClouds').classList.add('leftMove');
-    document.querySelector('#middle').classList.add('middleMove');
-    document.querySelector('#topRightClouds').classList.add('rightMove');
-    document.querySelector('#bottomRightClouds').classList.add('rightMove');
-    document.querySelector('p').classList.add('opacity');
-    document.querySelector('button').classList.add('opacity');
+    document.querySelector('p').classList.add('textOpacity');
+    document.querySelector('button').classList.add('textOpacity');
+    document.querySelector('#cloud1').classList.add('cloud1');
+    document.querySelector('#cloud2').classList.add('cloud2');
+    document.querySelector('#cloud3').classList.add('cloud3');
+    document.querySelector('#cloud4').classList.add('cloud4');
+    document.querySelector('#cloud5').classList.add('cloud5');
+    document.querySelector('#cloud6').classList.add('cloud6');
+    document.querySelector('#cloud7').classList.add('cloud7');
+    document.querySelector('#cloud8').classList.add('cloud8');
+    document.querySelector('#cloud9').classList.add('cloud9');
+    document.querySelector('#cloud10').classList.add('cloud10');
+    document.querySelector('#cloud11').classList.add('cloud11');
+    document.querySelector('#cloud12').classList.add('cloud12');
+    document.querySelector('#cloud13').classList.add('cloud13');
+    document.querySelector('#cloud14').classList.add('cloud14');
+    document.querySelector('#cloud15').classList.add('cloud15');
+    document.querySelector('#cloud16').classList.add('cloud16');
 });
 
 function delay (URL) {
-    setTimeout( function() { window.location = URL }, 3500);
+    setTimeout( function() { window.location = URL }, 2500);
 }
