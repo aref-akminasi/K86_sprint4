f79ccb7e3a5a03572c8c319d47c0eff894728aa0
Thijsvdzn <78796431+Thijsvdzn@users.noreply.github.com>
2022-06-23 14:07:27 +0200

    Town Upgraded

--- js/town.js
+++ js/town.js
@@ -1,44 +1,54 @@
 // Hover state on Thijs' building
 document.getElementById('thijsBuilding').onmouseenter = function() {
     document.getElementById('thijsPlein').style.display = "block";
+    document.getElementById('thijsGloed').style.opacity = "1.0";
 }
 
 document.getElementById('thijsBuilding').onmouseleave = function() {
     document.getElementById('thijsPlein').style.display = "none";
+    document.getElementById('thijsGloed').style.opacity = "0.0";
 }
 
 //Hover state on Aref's building
 document.getElementById('arefBuilding').onmouseenter = function() {
     document.getElementById('arefHof').style.display = "block";
+    document.getElementById('arefGloed').style.opacity = "1.0";
 }
 
 document.getElementById('arefBuilding').onmouseleave = function() {
     document.getElementById('arefHof').style.display = "none";
+    document.getElementById('arefGloed').style.opacity = "0.0";
 }
 
 //Hover state on Maartje's building
 document.getElementById('maartjeBuilding').onmouseenter = function() {
     document.getElementById('maartjeLaan').style.display = "block";
+    document.getElementById('maartjeGloed').style.opacity = "1.0";
 }
 
 document.getElementById('maartjeBuilding').onmouseleave = function() {
     document.getElementById('maartjeLaan').style.display = "none";
+    document.getElementById('maartjeGloed').style.opacity = "0.0";
 }
 
 //Hover state on Tom's building
 document.getElementById('tomBuilding').onmouseenter = function() {
     document.getElementById('tomStraat').style.display = "block";
+    document.getElementById('tomGloed').style.opacity = "1.0";
 }
 
 document.getElementById('tomBuilding').onmouseleave = function() {
     document.getElementById('tomStraat').style.display = "none";
+    document.getElementById('tomGloed').style.opacity = "0.0";
 }
 
 //Hover state on Maud's building
 document.getElementById('maudBuilding').onmouseenter = function() {
     document.getElementById('maudGracht').style.display = "block";
+    document.getElementById('maudGloed').style.opacity = "1.0";
 }
 
 document.getElementById('maudBuilding').onmouseleave = function() {
     document.getElementById('maudGracht').style.display = "none";
+    document.getElementById('maudGloed').style.opacity = "0.0";
 }
