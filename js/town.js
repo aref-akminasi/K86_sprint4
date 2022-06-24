1fd4671fdf463518a03fbbc443f8a7679fa4dff1
Thijsvdzn <78796431+Thijsvdzn@users.noreply.github.com>
2022-06-24 13:14:34 +0200

    Town updated

--- js/town.js
+++ js/town.js
@@ -1,54 +1,54 @@
 // Hover state on Thijs' building
 document.getElementById('thijsBuilding').onmouseenter = function() {
-    document.getElementById('thijsPlein').style.display = "block";
+    document.getElementById('thijsPlein').style.opacity = "1.0";
     document.getElementById('thijsGloed').style.opacity = "1.0";
 }
 
 document.getElementById('thijsBuilding').onmouseleave = function() {
-    document.getElementById('thijsPlein').style.display = "none";
+    document.getElementById('thijsPlein').style.opacity = "0.0";
     document.getElementById('thijsGloed').style.opacity = "0.0";
 }
 
 //Hover state on Aref's building
 document.getElementById('arefBuilding').onmouseenter = function() {
-    document.getElementById('arefHof').style.display = "block";
+    document.getElementById('arefHof').style.opacity = "1.0";
     document.getElementById('arefGloed').style.opacity = "1.0";
 }
 
 document.getElementById('arefBuilding').onmouseleave = function() {
-    document.getElementById('arefHof').style.display = "none";
+    document.getElementById('arefHof').style.opacity = "0.0";
     document.getElementById('arefGloed').style.opacity = "0.0";
 }
 
 //Hover state on Maartje's building
 document.getElementById('maartjeBuilding').onmouseenter = function() {
-    document.getElementById('maartjeLaan').style.display = "block";
+    document.getElementById('maartjeLaan').style.opacity = "1.0";
     document.getElementById('maartjeGloed').style.opacity = "1.0";
 }
 
 document.getElementById('maartjeBuilding').onmouseleave = function() {
-    document.getElementById('maartjeLaan').style.display = "none";
+    document.getElementById('maartjeLaan').style.opacity = "0.0";
     document.getElementById('maartjeGloed').style.opacity = "0.0";
 }
 
 //Hover state on Tom's building
 document.getElementById('tomBuilding').onmouseenter = function() {
-    document.getElementById('tomStraat').style.display = "block";
+    document.getElementById('tomStraat').style.opacity = "1.0";
     document.getElementById('tomGloed').style.opacity = "1.0";
 }
 
 document.getElementById('tomBuilding').onmouseleave = function() {
-    document.getElementById('tomStraat').style.display = "none";
+    document.getElementById('tomStraat').style.opacity = "0.0";
     document.getElementById('tomGloed').style.opacity = "0.0";
 }
 
 //Hover state on Maud's building
 document.getElementById('maudBuilding').onmouseenter = function() {
-    document.getElementById('maudGracht').style.display = "block";
+    document.getElementById('maudGracht').style.opacity = "1.0";
     document.getElementById('maudGloed').style.opacity = "1.0";
 }
 
 document.getElementById('maudBuilding').onmouseleave = function() {
-    document.getElementById('maudGracht').style.display = "none";
+    document.getElementById('maudGracht').style.opacity = "0.0";
     document.getElementById('maudGloed').style.opacity = "0.0";
 }
